6f6eefa1b83520b15ef0bb68c21282bc867891ed
Francisco Andrade <franciscoezequielandrade@gmail.com>
2022-05-26 10:24:47 -0300

    agregamos y modificamos elementos de arreglos

--- 11-javascrypt/js/5-arrays.js
+++ 11-javascrypt/js/5-arrays.js
@@ -10,3 +10,38 @@ document.write('<br>' + series[4]);
 for (let i = 0; i < series.length; i++) {
     document.write('<br>' + series[i]);
 }
+
+// Agregar un elemento al final del arreglo
+document.write('<hr>');
+series.push('Ozark');
+for (let i = 0; i < series.length; i++) {
+    document.write('<br>' + series[i]);
+}
+
+// Agragar un elemento en una posicion particuar
+document.write('<hr>');
+series.splice(3,0,'Breaking Bad');
+for (let i = 0; i < series.length; i++) {
+    document.write('<br>' + series[i]);
+}
+
+// Elimar un elemento desde uns posicion particular
+// document.write('<hr>');
+// series.splice(5,2); //Elimina desde la posicion 5 dos elementos
+// for (let i = 0; i < series.length; i++) {
+//     document.write('<br>' + series[i]);
+// }
+
+// Elimar todos los elementos desde una posicion particular
+document.write('<hr>');
+series.splice(7);
+for (let i = 0; i < series.length; i++) {
+    document.write('<br>' + series[i]);
+}
+
+// Modificar el elemento de un arreglo
+series[6] = true;
+document.write('<hr>');
+for (let i = 0; i < series.length; i++) {
+    document.write('<br>' + series[i]);
+}
