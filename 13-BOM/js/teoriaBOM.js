a0a05bf92aa6ace6c4346da4837bec0d479d9afb
Francisco Andrade <franciscoezequielandrade@gmail.com>
2022-07-04 19:32:40 -0300

    se agrego la funcgo la funcionalidad del reloj

--- 13-BOM/js/teoriaBOM.js
+++ 13-BOM/js/teoriaBOM.js
@@ -1,12 +1,12 @@
-let contador = 1;
+// let contador = 1;
 
-function mostrarPublicidad(){
-    if (contador === 5){
-        window.clearInterval(codigo);
-    }
-    document.write(`<p>Ahora muestro publicidad<\p>`);
-    contador++;
-}
+// function mostrarPublicidad(){
+//     if (contador === 5){
+//         window.clearInterval(codigo);
+//     }
+//     document.write(`<p>Ahora muestro publicidad<\p>`);
+//     contador++;
+// }
 
 // ejecutar una funcion pasado cierto tiempo en milisegundos
 // window.setTimeout(mostrarPublicidad, 3000); //va sin parentesis las funciones
@@ -24,5 +24,5 @@ function mostrarPublicidad(){
 
 
 // ejecutar una funcion cada cierto tiempo en milisegundos
-let codigo = window.setInterval(mostrarPublicidad,3000); //Asi guardamos un identificador
+// let codigo = window.setInterval(mostrarPublicidad,3000); //Asi guardamos un identificador
 
